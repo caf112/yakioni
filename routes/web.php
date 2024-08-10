fe785def1ef68d55fa79f2f472334e528c52d999
caf112 <yuu10san.14@gmail.com>
2024-08-10 18:16:03 +0900

    viewsを作成。それに伴ってrutes追加

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
@@ -11,4 +12,5 @@ Route::get('/', function () {
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/home', [HomeController::class, 'index'])->name('home');
     Route::resource('/articles', ArticleController::class);
+    Route::resource('/companys', CompanyController::class);
 });
