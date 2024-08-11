2f12201c1465f8f8b35f2ce0eb3653321017f288
caf112 <yuu10san.14@gmail.com>
2024-08-11 14:11:30 +0900

    onigiri

---
+++ resources/views/articles/yakioni.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+@section('content')
+<article class="article-yakioni">
+    <img src="/1.jpg" alt="Description of image" class="y-image">
+    <img src="/2.jpg" alt="Description of image" class="y-image">
+    <img src="/3.jpg" alt="Description of image" class="y-image">
+    <img src="/4.jpg" alt="Description of image" class="y-image">
+    <img src="/5.jpg" alt="Description of image" class="y-image">
+    <img src="/6.jpg" alt="Description of image" class="y-image">
+    <img src="/7.jpg" alt="Description of image" class="y-image">
+    <img src="/8.jpg" alt="Description of image" class="y-image">
+    <img src="/9.jpg" alt="Description of image" class="y-image">
+    <img src="/10.jpg" alt="Description of image" class="y-image">
+    <img src="/11.jpg" alt="Description of image" class="y-image">
+    <img src="/12.jpg" alt="Description of image" class="y-image">
+    <img src="/13.jpg" alt="Description of image" class="y-image">
+    <img src="/14.jpg" alt="Description of image" class="y-image">
+    <img src="/15.jpg" alt="Description of image" class="y-image">
+    <img src="/16.jpg" alt="Description of image" class="y-image">
+    <img src="/17.jpg" alt="Description of image" class="y-image">
+    <img src="/18.jpg" alt="Description of image" class="y-image">
+    <img src="/19.jpg" alt="Description of image" class="y-image">
+    <img src="/20.jpg" alt="Description of image" class="y-image">
+    <img src="/21.jpg" alt="Description of image" class="y-image">
+    <img src="/22.jpg" alt="Description of image" class="y-image">
+    <img src="/23.jpg" alt="Description of image" class="y-image">
+    <img src="/24.jpg" alt="Description of image" class="y-image">
+    <img src="/25.jpg" alt="Description of image" class="y-image">
+</article>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,6 @@
 @section('content')
 <h1 class="page-heading">マイページ</h1>
 <p>ようこそ、{{ Auth::user()->name }}さん｜<a href="{{ route('articles.create') }}">記事を書く</a></p>
+<a href="https://calendar.google.com/" class="button">Googleカレンダーへ</a>
 @include('articles.articles')
 @endsection()
