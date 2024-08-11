f0347096408ff2bef9d5507ba8b21265b1ec2c72
caf112 <yuu10san.14@gmail.com>
2024-08-11 15:08:30 +0900

    editのカレンダー部分を削除

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -3,6 +3,14 @@
 @include('commons.errors')
 <form action="{{ route('articles.store') }}" method="post">
     @include('articles.form')
+    <dt>イベント名<sup class="red">*</sup></dt>
+    <dd><input type="text" name="summary" value="{{ old('summary') }}"></dd>
+
+    <dt>開始日時<sup class="red">*</sup></dt>
+    <dd><input type="datetime-local" name="start_date" value="{{ old('start_date') }}"></dd>
+
+    <dt>終了日時<sup class="red">*</sup></dt>
+    <dd><input type="datetime-local" name="end_date" value="{{ old('end_date') }}"></dd>
     <button type="submit">投稿する</button>
     <a href="{{ route('articles.index') }}">キャンセル</a>
 </form>
--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -19,13 +19,6 @@
     <dt>メモ<sup class="red">*</sup></dt>
     <dd><textarea name="body" rows="5">{{ old('body', $article->body) }}</textarea></dd>
 
-    <dt>イベント名<sup class="red">*</sup></dt>
-    <dd><input type="text" name="summary" value="{{ old('summary') }}"></dd>
-
-    <dt>開始日時<sup class="red">*</sup></dt>
-    <dd><input type="datetime-local" name="start_date" value="{{ old('start_date') }}"></dd>
-
-    <dt>終了日時<sup class="red">*</sup></dt>
-    <dd><input type="datetime-local" name="end_date" value="{{ old('end_date') }}"></dd>
+    
 
 </dl>
