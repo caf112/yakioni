65d35f52f927a4d45b8fefe8964663c629314b7f
caf112 <yuu10san.14@gmail.com>
2024-08-12 21:07:15 +0900

    _blank

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,6 @@
 @section('content')
 <h1 class="page-heading">マイページ</h1>
 <p>ようこそ、{{ Auth::user()->name }}さん｜<a href="{{ route('articles.create') }}">記事を書く</a></p>
-<a href="https://calendar.google.com/" class="button">Googleカレンダーへ</a>
+<a href="https://calendar.google.com/" class="button" target="_blank">Googleカレンダーへ</a>
 @include('articles.articles')
 @endsection()
